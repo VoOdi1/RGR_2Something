44afc16d962912a3c42c896e7756763e129ed11f
Ilya1 <ilya.balashow1@yandex.ru>
2019-06-05 08:21:25 +0700

    something

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class Controller {
 
@@ -9,7 +9,7 @@ public class Controller {
     public Controller(File txt2) {
         this.txt2=txt2;
     }
-
+    Controller(){};
     public void read_file_txt(File txt2) throws IOException {
 
         this.txt2=txt2;
@@ -53,48 +53,65 @@ public class Controller {
 
         }
 
-    public String codeString (String input){
-        char[][] matrix = new char[11][11];
+    public String codeString1 (String input){
+        char[][] matrix = new char[3][3];
 
-        iterateCodeMatrix (matrix, 5, 5, true, 0, input);
+        iterateCodeMatrix1 (matrix, 1, 1, true, 0, input);
 
         String output = "";
         for (int i = 0; i < matrix.length; i++){
             for (int j = 0; j < matrix[0].length; j++){
                 output += matrix[j][i];
+
             }
+
         }
 
 
         return output;
     }
-    private void iterateCodeMatrix (char[][] matrix, int x, int y, boolean goRight, int length, String s){
-        /* This recursive method fills the matrix this way:
-         * (n is a number of an iteration)
-         *  4 4 4 4 3
-         *  4 2 2 1 3
-         *  4 2 0 1 3
-         *  4 2 3 3 3
-         *  4 5 5 5 5
-         */
+    public String codeString2 (String output){
+        char[][] matrix2 = new char[3][3];
 
-        int iterator = 0;
+        iterateCodeMatrix2 (matrix2, 1, 1, true, 0, output);
+
+        String input = "";
+        for (int i = 0; i < matrix2.length; i++){
+            for (int j = 0; j < matrix2[0].length; j++){
+                input += matrix2[j][i];
+
+            }
+
+        }
+
+
+        return input;
+    }
+
+
+
+
+
+
+    private void iterateCodeMatrix1 (char[][] matrix1, int x, int y, boolean goRight, int length, String s){
+
+        int iterator1 = 0;
 
         // if first iteration
         if (length == 0){
-            matrix[x][y] = s.charAt(iterator);
-            iterateCodeMatrix(matrix, x + 1, y, goRight, length + 1, s.substring(1));
+            matrix1[x][y] = s.charAt(iterator1);
+            iterateCodeMatrix1(matrix1, x + 1, y, goRight, length + 1, s.substring(1));
         }
         //if last iteration
-        else if (length == matrix.length){
-            for (int i = 1; i < matrix.length; i++){
-                matrix[i][y] = s.charAt(iterator);
-                iterator++;
+        else if (length == matrix1.length){
+            for (int i = 1; i < matrix1.length; i++){
+                matrix1[i][y] = s.charAt(iterator1);
+                iterator1++;
             }
 
-            for (int i = 0; i < matrix.length; i++){
-                for (int j = 0; j < matrix[0].length; j++){
-                    System.out.print(matrix[j][i] + " ");
+            for (int i = 0; i < matrix1.length; i++){
+                for (int j = 0; j < matrix1[0].length; j++){
+                    System.out.print(matrix1[j][i] + " ");
                 }
                 System.out.print("\n");
             }
@@ -104,35 +121,98 @@ public class Controller {
             if (goRight){
                 //go right
                 for (int i = 0; i < length; i++){
-                    matrix[x + i][y] = s.charAt(iterator);
-                    iterator++;
+                    matrix1[x + i][y] = s.charAt(iterator1);
+                    iterator1++;
                 }
                 //go up
                 for (int j = 0; j < length; j++){
-                    matrix[x + length - 1][y - j - 1] = s.charAt(iterator);
-                    iterator++;
+                    matrix1[x + length - 1][y - j - 1] = s.charAt(iterator1);
+                    iterator1++;
                 }
                 //next iteration
-                iterateCodeMatrix(matrix, x + length - 2, y - length, !goRight, length + 1, s.substring(length * 2));
+                iterateCodeMatrix1(matrix1, x + length - 2, y - length, !goRight, length + 1, s.substring(length * 2));
             }
             else if (!goRight){
                 //go left
+
                 for (int i = 0; i < length; i++){
-                    matrix[x - i][y] = s.charAt(iterator);
-                    iterator++;
+                    matrix1[x - i][y] = s.charAt(iterator1);
+                    iterator1++;
                 }
                 //go down
                 for (int j = 0; j < length; j++){
-                    matrix[x - length + 1][y + j + 1] = s.charAt(iterator);
-                    iterator++;
+                    matrix1[x - length + 1][y + j + 1] = s.charAt(iterator1);
+                    iterator1++;
                 }
                 //next iteration
-                iterateCodeMatrix(matrix, x - length + 2, y + length, !goRight, length + 1, s.substring(length * 2));
+                iterateCodeMatrix1( matrix1, x - length + 2, y + length, !goRight, length + 1, s.substring(length * 2));
             }
 
         }
 
     }
+    private void iterateCodeMatrix2 (char[][] matrix2, int y, int x, boolean goRight, int length, String s) {
+        int iterator1 = 0;
+        char[][] matrix = new char[3][3];
+        int x1=0,y1=0;
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[y1][x1] = s.charAt(iterator1);
+            }
+            }
+
+        if (length == 0){
+            matrix2[y1][x1] =  matrix[y][x];
+        iterateCodeMatrix1(matrix2, y, x+1, goRight, length + 1, s.substring(1));
+        }
+        else if (length == matrix2.length){
+
+            y1++;
+            for(int i=0; i<length;i++)
+                matrix2[y1][i] = matrix[y][x];
+
+
+
+            for (int i = 0; i < matrix2.length; i++){
+                for (int j = 0; j < matrix2[0].length; j++) {
+                    System.out.print(matrix2[i][j]);
+                }
+            }
+        }
+
+        else {
+            if (goRight) {
+                x1++;
+                matrix2[y1][x1] = matrix[y][x];
+
+                x1++;
+                matrix2[y1][x1] = matrix[y - 1][x];
+
+                iterateCodeMatrix1(matrix2, y, x - 1, !goRight, length + 1, s.substring(length * 2));
+            }
+            else if (!goRight) {
+                y1++;
+                x1--;
+                x1--;
+                matrix2[y1][x1] = matrix[y][x];
+                x1++;
+                matrix2[y1][x1] = matrix[y][x - 1];
+                x1++;
+                matrix2[y1][x1] = matrix[y+1][x];
+                iterateCodeMatrix1(matrix2, y , x, goRight, 9, s.substring(6));
+
+
+
+            }
+
+        }// if first iteration
+
+    }
+
+
+
+
+
 
 
 
